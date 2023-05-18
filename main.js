1f758601930ff01c5d9e7150c4d9893fb8093143
Bubu <50858626+BubuDavid@users.noreply.github.com>
2023-05-18 12:06:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,51 @@
-document.querySelector('#upload-form').addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevents the form from submitting the default way
+document.querySelector('#upload-form').addEventListener('submit', sendRequest);
 
-  const fileInput = document.querySelector('#file');
+
+function sendRequest(e) {
+	e.preventDefault(); // Prevents the form from submitting the default way
+
+	const fileInput = document.querySelector('#file');
 	const file = fileInput.files[0];
 	
 	if (file) {
 		const reader = new FileReader();
-		reader.onload = function(e) {
-				const image = document.createElement('img');
-				image.src = e.target.result;
-				image.width = 200;  // You can adjust the size as needed
-				const displayDiv = document.getElementById('image-display');
-				// Clear out any old images
-				displayDiv.innerHTML = '';
-				// Append the new image
-				displayDiv.appendChild(image);
+		reader.onload = function (e) {
+			const image = document.createElement('img');
+			image.src = e.target.result;
+			image.width = 200;  // You can adjust the size as needed
+			const displayDiv = document.getElementById('image-display');
+			// Clear out any old images
+			displayDiv.innerHTML = '';
+			// Append the new image
+			displayDiv.appendChild(image);
 		};
 		reader.readAsDataURL(file);
-}
+	}
 
-  // Create a FormData object and append the file
-  const formData = new FormData();
-  formData.append('file', file);
+	// Create a FormData object and append the file
+	const formData = new FormData();
+	formData.append('file', file);
 	// Show the modal
 	document.getElementById('loadingModal').style.display = 'block';
   
 	// Make the request to the FastAPI server
-  fetch('http://localhost:8000/cat-vs-dog/', {
-    method: 'POST',
-    body: formData
-  })
-    .then(response => response.json())
-    .then(data => {
+	fetch('http://localhost:8000/cat-vs-dog/', {
+		method: 'POST',
+		body: formData
+	})
+		.then(response => response.json())
+		.then(data => {
 			// Hide the modal
-			document.getElementById('loadingModal').style.display = 'none';			
+			document.getElementById('loadingModal').style.display = 'none';
 			// Display the classification
 			classification = data['classification']
 			msg = document.getElementById('classification-msg')
 			msg.innerText = classification
 			msg.style.display = 'block'
 
-    })
-    .catch((error) => {
+		})
+		.catch((error) => {
 			console.error('Error:', error);
-			document.getElementById('errorModal').style.display = 'block';	
-    });
-});
+			document.getElementById('errorModal').style.display = 'block';
+		});
+}
